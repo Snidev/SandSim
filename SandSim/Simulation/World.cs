d8ee74681a166f8a6c6dde0c4d74541c613576ce
jrgerba <119472456+Snidev@users.noreply.github.com>
2025-01-27 10:00:53 -0500

    Basic falling physics

--- SandSim/Simulation/World.cs
+++ SandSim/Simulation/World.cs
@@ -4,6 +4,9 @@ public class World(int width, int height)
 {
     public readonly int Width = width;
     public readonly int Height = height;
+
+    // Quick and dirty solution, optimize later
+    private readonly HashSet<(int, int)> _noUpdate = new();
     
     private int[,] _dots = new int[width, height];
 
@@ -13,9 +16,14 @@ public class World(int width, int height)
     
     public void Update()
     {
+        _noUpdate.Clear();
+        
         for (int x = 0; x < Width; x++)
         for (int y = 0; y < Height; y++)
         {
+            if (_noUpdate.Contains((x, y)))
+                continue;
+            
             int curDot = GetDot(x, y);
 
             if (curDot == 1) // Sand, falls down
@@ -23,11 +31,13 @@ public class World(int width, int height)
                 if (y >= Height - 1)
                     continue;
                 
-                if (GetDot(x, y+1) != 0)
+                int newX = x, newY = y + 1;
+                if (GetDot(newX, newY) != 0)
                     continue;
-                    
+                
                 SetDot(x, y, 0);
-                SetDot(x, y+1, 1);
+                SetDot(newX, newY, 1);
+                _noUpdate.Add((newX, newY));
             }
         }
     }
